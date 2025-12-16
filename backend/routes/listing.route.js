d74d0f8ec43de360b9b0aa3d2f063f49dc88afac
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-16 19:17:18 +0530

    "Created a create route for adding the data in the db"

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -9,16 +9,30 @@ router.get("/listings", async (req, res) => {
     res.render("listings/index.ejs", { listingData });
     // console.log(listingData);
   } catch (error) {
-    console.log("Issue inside /listing route inside listing.route.js");
+    console.log("Issue inside /listings route(index route)");
     console.error("Error: ", error);
   }
 });
 
 // form(get method) for adding new data
-router.get("/listings/new", (req, res)=>{
-    res.render("listings/new.ejs")
-})
-// read route
+router.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// form(post) for adding new data
+router.post("/listings", async (req, res) => {
+  try {
+    let newlisting = await new Listing(req.body.listing);
+    await newlisting.save();
+    res.redirect("/listings");
+    console.log(newlisting);
+  } catch (error) {
+    console.log("Error in /listing route of post method");
+    console.error(error);
+  }
+});
+
+// read route(show single data)
 router.get("/listings/:_id", async (req, res) => {
   try {
     let { _id } = req.params;
@@ -30,6 +44,5 @@ router.get("/listings/:_id", async (req, res) => {
     console.error("Error: ", error);
   }
 });
-
 
 module.exports = router;
