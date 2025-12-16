b4ed4cd6ce7303f487042863831797ea2a0e3663
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-16 16:53:00 +0530

    "Created show route"

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -3,15 +3,28 @@ const router = express.Router();
 const Listing = require("../models/listing");
 
 // index route
-router.get("/listing", async (req, res) => {
+router.get("/listings", async (req, res) => {
   try {
     let listingData = await Listing.find();
-    res.render("listings/index.ejs", {listingData})
-    console.log(listingData);
+    res.render("listings/index.ejs", { listingData });
+    // console.log(listingData);
   } catch (error) {
     console.log("Issue inside /listing route inside listing.route.js");
     console.error("Error: ", error);
   }
 });
 
-module.exports = router
+// read route
+router.get("/listings/:_id", async (req, res) => {
+  try {
+    let { _id } = req.params;
+    let idData = await Listing.findById(_id);
+    console.log("ID data: ", idData);
+    res.render("listings/show.ejs", { idData });
+  } catch (error) {
+    console.log("Issue inside /listing/:_id route inside listing.route.js");
+    console.error("Error: ", error);
+  }
+});
+
+module.exports = router;
