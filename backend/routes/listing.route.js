552224cef53aa869d436eabb579371542b1d8b14
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-12-16 16:32:40 +0530

    "Created index route"

--- backend/routes/listing.route.js
+++ backend/routes/listing.route.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing");
 
+// index route
 router.get("/listing", async (req, res) => {
   try {
     let listingData = await Listing.find();
-    res.render("show.ejs", {listingData})
+    res.render("listings/index.ejs", {listingData})
     console.log(listingData);
   } catch (error) {
     console.log("Issue inside /listing route inside listing.route.js");
